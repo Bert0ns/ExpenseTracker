0dd8ae831fb193c3cc5372fd22c5babfe94f9242
Bert0ns <121551954+Bert0ns@users.noreply.github.com>
2024-04-23 19:22:28 +0200

    update

--- src/main/java/com/bertons/expensetracker/App.java
+++ src/main/java/com/bertons/expensetracker/App.java
@@ -1,23 +1,45 @@
 package com.bertons.expensetracker;
 
+import com.bertons.expensetracker.controller.LoginController;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class App extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+    private static final String JDBC_Driver = "org.postgresql.Driver";
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/jdbc_schema?user=user&password=secret&ssl=false";
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        HikariDataSource hikariDataSource = initDataSource(JDBC_Driver, JDBC_URL);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login-view.fxml"));
+        Parent root = fxmlLoader.load();
+        LoginController controller = fxmlLoader.getController();
+        controller.initDataSource(hikariDataSource);
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Hello! Login in the credential manager");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private HikariDataSource initDataSource(String JDBC_Driver, String JDBC_URL) {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(JDBC_Driver);
+        config.setJdbcUrl(JDBC_URL);
+        config.setLeakDetectionThreshold(2000);
+        return new HikariDataSource(config);
     }
 }
